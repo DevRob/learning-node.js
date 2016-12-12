2a5f67dd799a73ffd1af263c2bb1f5cb79de4127
Robert Molnar <mail.robertmolnar@gmail.com>
2016-12-12 17:55:55 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var _ = require("lodash");
+var axios = require("axios");
+
+axios.get("http://rest.learncode.academy/api/myuser/friends").then((res) => {
+  var jake = _.find(res.data, {name: "Jake"});
+  console.log(jake);
+});
+
+// console.log(_.snakeCase("SomeValue"));
